fb8a93502e1acee06a99a1f86f96e478796c5a71
赵忍德 <351157970@qq.com>
2017-02-12 21:20:21 +0800

    清空文件

--- src/adminlteng2/app/layout/mainsidebar.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    moduleId:module.id,
-    selector: 'app-main-sidebar',
-    templateUrl: `main_sidebar.html`
-})
-export class MainSidebarComponent { }
